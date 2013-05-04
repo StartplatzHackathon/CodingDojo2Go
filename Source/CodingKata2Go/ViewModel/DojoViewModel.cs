7c0ee574d84c1b6f7e5da7370bb7c3969d547372
unknown <wickelr@dentw-nb002.bewotec.local>
2013-05-04 19:46:06 +0200

    add service call

--- Source/CodingKata2Go/ViewModel/DojoViewModel.cs
+++ Source/CodingKata2Go/ViewModel/DojoViewModel.cs
@@ -1,5 +1,8 @@
-﻿using CodingKata2Go.DataModel;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using CodingKata2Go.DataModel;
 using CodingKata2Go.Services;
+using CodingKata2Go.WebServiceReferences;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Microsoft.Practices.ServiceLocation;
@@ -9,17 +12,27 @@ namespace CodingKata2Go.ViewModel
     public class DojoViewModel : ViewModelBase
     {
         private readonly DojoStateMachine m_dojoStateMachine;
+        private readonly UserService m_userService;
+        private ObservableCollection<CompileError> _compileErrors;
+        private ObservableCollection<TestError> _testErrors;
         private string m_code;
         private string m_test;
 
         public DojoViewModel()
         {
             m_dojoStateMachine = ServiceLocator.Current.GetInstance<DojoStateMachine>();
-            m_dojoStateMachine.AddAndActivate(new DojoState{ IsTestEnabled = true, IsFightEnabled = true});
+            m_dojoStateMachine.AddAndActivate(new DojoState
+                {
+                    IsTestEnabled = true,
+                    IsFightEnabled = true
+                });
             m_userService = ServiceLocator.Current.GetInstance<UserService>();
             FightCommand = new RelayCommand(Fight);
             FightBackCommand = new RelayCommand(FightBack);
             VerifyCommand = new RelayCommand(Verify);
+
+            _compileErrors = new ObservableCollection<CompileError>();
+            _testErrors = new ObservableCollection<TestError>();
         }
 
         public string Test
@@ -28,7 +41,6 @@ namespace CodingKata2Go.ViewModel
             set { Set(() => Test, ref m_test, value); }
         }
 
-        private readonly UserService m_userService;
         public string Code
         {
             get { return m_code; }
@@ -39,23 +51,64 @@ namespace CodingKata2Go.ViewModel
         public RelayCommand FightBackCommand { get; private set; }
         public RelayCommand VerifyCommand { get; private set; }
 
+        public ObservableCollection<CompileError> CompileErrors
+        {
+            get { return _compileErrors; }
+            set { Set(() => CompileErrors, ref _compileErrors, value); }
+        }
+
+        public ObservableCollection<TestError> TestErrors
+        {
+            get { return _testErrors; }
+            set { Set(() => TestErrors, ref _testErrors, value); }
+        }
+
         public void Fight()
         {
-            var nextState = new DojoState { IsCodeEnabled = true };
+            var nextState = new DojoState
+                {
+                    IsCodeEnabled = true
+                };
             m_dojoStateMachine.AddAndActivate(nextState);
         }
 
         public void FightBack()
         {
-            m_dojoStateMachine.AddAndActivate(new DojoState { IsTestEnabled = true, IsFightEnabled = true });
+            m_dojoStateMachine.AddAndActivate(new DojoState
+                {
+                    IsTestEnabled = true,
+                    IsFightEnabled = true
+                });
             m_userService.SwitchUsers();
         }
 
-        public void Verify()
+        public async void Verify()
         {
-            // Todo: Webrequest.
-            var nextState = new DojoState { CodeState = CodeState.Pass, IsCodeEnabled = true, IsFightBackEnabled = true };
-            m_dojoStateMachine.AddAndActivate(nextState);
+            var client = new ServiceClient();
+            CompileAndTestResult result = await client.CompileAndTestAsync(new KataRequest());
+
+            if (result.CompileErrors.Any() || result.TestErrors.Any())
+            {
+                var nextState = new DojoState
+                    {
+                        CodeState = CodeState.Fail,
+                        IsCodeEnabled = true
+                    };
+                m_dojoStateMachine.AddAndActivate(nextState);
+            }
+            else
+            {
+                var nextState = new DojoState
+                    {
+                        CodeState = CodeState.Pass,
+                        IsCodeEnabled = true,
+                        IsFightBackEnabled = true
+                    };
+                m_dojoStateMachine.AddAndActivate(nextState);
+            }
+
+            CompileErrors = new ObservableCollection<CompileError>(result.CompileErrors);
+            TestErrors = new ObservableCollection<TestError>(result.TestErrors);
         }
     }
 }
