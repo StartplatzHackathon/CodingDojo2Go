1676bfb86c5b6f60088df7c7b2070685008bb9b1
Ricardo <ricardo.wickel@gmail.com>
2013-05-04 12:22:58 +0200

    initial creation

---
+++ Source/CodingKata2Go.WebServices/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace CodingKata2Go.WebServices.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ Source/CodingKata2Go.WebServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingKata2Go.WebServices.WebApiApplication" Language="C#" %>
