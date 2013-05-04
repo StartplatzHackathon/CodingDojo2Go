8c0c584c8792efb4c5caa6b68025e14ca7e07f2c
FourSpotProject <bernhard.thielen@fourspotproject.com>
2013-05-04 17:24:33 +0200

    Dojo StateMachine started

--- Source/CodingKata2Go/ViewModel/DojoViewModel.cs
+++ Source/CodingKata2Go/ViewModel/DojoViewModel.cs
@@ -9,6 +9,34 @@ namespace CodingKata2Go.ViewModel
 {
     public class DojoViewModel : ViewModelBase
     {
+        private string m_test;
 
+        public string Test
+        {
+            get { return m_test; }
+            set
+            {
+                if (m_test != value)
+                {
+                    m_test = value;
+                    RaisePropertyChanged(() => Test);
+                }
+            }
+        }
+
+        private string m_code;
+
+        public string Code
+        {
+            get { return m_code; }
+            set
+            {
+                if (m_code != value)
+                {
+                    m_code = value;
+                    RaisePropertyChanged(() => Code);
+                }
+            }
+        }
     }
 }
